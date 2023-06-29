40a9fd144c71c47834098b52e78e1dea395a2611
BlankParticle <blankparticle@gmail.com>
2023-06-29 20:43:25 +0530

    feat: Prisma flake Template & .envrc for each template

--- templates/default.nix
+++ templates/default.nix
@@ -3,6 +3,10 @@
     path = ./nodejs;
     description = "NodeJS current and pnpm Project";
   };
+  nodejs-prisma = {
+    path = ./nodejs-prisma;
+    description = "NodeJS current, pnpm & prisma Project";
+  };
   tauri = {
     path = ./tauri;
     description = "Tauri Project";
---
+++ templates/nodejs-prisma/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NodeJS current, pnpm & prisma Project";
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodePackages.prisma
+            nodePackages.pnpm
+            nodejs_20
+          ];
+          shellHook = with pkgs; ''
+            export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          '';
+        };
+      });
+}
