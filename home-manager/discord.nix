ecbf68aca8ba873b33176186a475ea4df504d446
BlankParticle <blankparticle@gmail.com>
2023-07-04 10:08:09 +0530

    💠 feat(discord): replace BetterDiscord with Vencord ans OpenASAR

--- home-manager/discord.nix
+++ home-manager/discord.nix
@@ -1,21 +1,11 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   home = {
     packages = with pkgs; [
-      discord
-      betterdiscordctl
+      (discord.override {
+        withOpenASAR = true;
+        withVencord = true;
+        withTTS = false;
+      })
     ];
-    file."${config.xdg.configHome}/BetterDiscord/themes/mocha.theme.css".text = ''
-       /**
-       * @name Catppuccin Mocha
-       * @author winston#0001
-       * @authorId 505490445468696576
-       * @version 0.2.0
-       * @description 🎮 Soothing pastel theme for Discord
-       * @website https://github.com/catppuccin/discord
-       * @invite r6Mdz5dpFc
-       * **/
-
-      @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css");
-    '';
   };
 }
